168b382ff5775868718b22872f55bdda06820bc5
anishkc12 <anish.kc@live.com>
2016-09-01 10:08:24 -0500

    added

--- Assignment/Assignment2/js/student.js
+++ Assignment/Assignment2/js/student.js
@@ -60,6 +60,42 @@ function traverseArray(event) {
     }
 
     htmlContents += '</table>';
-    document.getElementById('a1').innerHTML = htmlContents;
+   // document.getElementById('a1').innerHTML = htmlContents;
+    $('#a1').html(htmlContents); //'#a1:selector,jquery syntax and same result.
 
 }
+
+// $(document).ready(function(){
+//     $('#btn1').click(function () {//associates button id with click event
+//         alert("hello world");
+//         addperson(event);
+//
+//     });
+// });
+
+
+$(document).ready(function(){
+$('#btn1').on('click', function () {//click:single click, dblclick:double click
+    alert("hello world");
+
+});
+    $('#first').hide();//hides the element.
+    $('#second').hide();
+    $('#firstbtn').on('click',function () {
+        $('#first').show();
+    });
+
+    $('#secondbtn').on('click',function () {
+        $('#second').show();
+    });
+
+    $('#thirdbtn').on('click',function () {
+        $('#firstbtn').trigger('click');
+        $('#secondbtn').trigger('click');
+
+
+    });
+});
+
+
+
